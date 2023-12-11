fb52a034e0f3273989115bec9c5e140a27889da9
Medo <kaled.h4@gmail.com>
2023-12-11 16:14:12 +0300

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -6,6 +6,7 @@ const url = `${base}&sheet=${sheetName}&tq=${query}`
 const data = []
 document.addEventListener('DOMContentLoaded', init)
 const output = document.querySelector('.output')
+
 function init() {
     fetch(url)
         .then(res => res.text())
@@ -34,10 +35,21 @@ function init() {
         })
 }
 
+function getColor(value) {
+    let hue;
+    if (value < 0) {
+        hue = 120; // Green for negative values
+    } else if (value <= 100) {
+        hue = 120 - value * 1.2; // Hue goes from 120 (green) to 0 (red) through 60 (white)
+    } else {
+        hue = 0; // Red for values above 100
+    }
+    return `hsl(${hue}, 100%, 90%)`; // Saturation is 100%, lightness is 90%
+}
 
 function processRows(json) {
     json.forEach((row, rowIndex) => {
-        if (rowIndex < 10) { // Only process the first 10 rows
+        if (rowIndex < 8) { // Only process the first 8 rows
             const tr = document.createElement('tr');
             const keys = Object.keys(row);
 
@@ -46,18 +58,20 @@ function processRows(json) {
                 let value = row[key];
                 if (columnIndex === 1) { // Column 2 (0-indexed)
                     value = parseFloat(value).toFixed(3); // Format as 0.000
-                    // Apply background color based on value
-                    if (value >= 0 && value <= 1) {
-                        const hue = 120 - value * 120; // Hue goes from 120 (green) to 0 (red) through 60 (white)
-                        td.style.backgroundColor = `hsl(${hue}, 100%, 90%)`; // Saturation is 100%, lightness is 90%
-                    }
+                    td.style.backgroundColor = getColor(value);
+                    td.textContent = value;
                 } else if (columnIndex === 3) { // Column 4 (0-indexed)
-                    value = parseFloat(value).toFixed(2) + '%'; // Format as percentage with two decimal places
+                    value = parseFloat(value) * 100; // Convert to percentage
+                    let formattedValue = value.toFixed(2) + '%'; // Format as percentage with two decimal places
+                    td.style.backgroundColor = getColor(value);
+                    td.textContent = formattedValue;
+                } else {
+                    td.textContent = value;
                 }
-                td.textContent = value;
                 tr.appendChild(td);
             })
             output.appendChild(tr);
         }
     })
 }
+
